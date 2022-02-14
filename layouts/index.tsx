e1626c88933a77c0ae6aa7f53694385cd4b44ef5
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-02-14 21:06:34 +0000

    added layout

---
+++ layouts/index.tsx
@@ -0,0 +1,16 @@
+// import Header from "../components/header";
+import styles from "./layout.module.scss";
+
+interface Props {
+	children: React.ReactNode;
+}
+
+const Layout = ({ children }: Props) => (
+	<>
+		{/* <Header /> */}
+		<main className={styles.main}>{children}</main>
+		<footer />
+	</>
+);
+
+export default Layout;
