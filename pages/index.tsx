22e54603b89304b550670758b01497e95927f0c6
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-02-15 23:55:04 +0000

    added another section

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,15 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import Title from "../components/title";
 import styles from "./index.module.scss";
-// import hero from "../public/hero.jpg";
 
 const Home: NextPage = () => {
 	return (
-		<div>
+		<>
+			<Head>
+				<title>Jobs</title>
+			</Head>
 			<section className={styles.hero}>
 				<div className={styles.hero__wrapper}>
 					<h1 className={styles.hero__title}>We&apos;re here to make hiring a little easier. Let&apos;s give it a try.</h1>
@@ -17,7 +20,10 @@ const Home: NextPage = () => {
 				</div>
 				<Image src="/hero.jpg" alt="hero image" priority layout="fill" objectFit="cover" quality={100} />
 			</section>
-		</div>
+			<section className={styles["section-wrapper"]}>
+				<Title>jobs hiring Now</Title>
+			</section>
+		</>
 	);
 };
 
