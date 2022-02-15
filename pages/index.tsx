da89187c06d2cea4e5b77f1b8b0eb421f1a03c6f
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-02-15 22:52:56 +0000

    added hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,24 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import variables from "../styles/_variables.module.scss";
+import Link from "next/link";
+import styles from "./index.module.scss";
+// import hero from "../public/hero.jpg";
 
 const Home: NextPage = () => {
-	return <div></div>;
+	return (
+		<div>
+			<section className={styles.hero}>
+				<div className={styles.hero__wrapper}>
+					<h1 className={styles.hero__title}>We&apos;re here to make hiring a little easier. Let&apos;s give it a try.</h1>
+					<Link href="/">
+						<a className={styles.hero__link}>post a job for free</a>
+					</Link>
+				</div>
+				<Image src="/hero.jpg" alt="hero image" priority layout="fill" objectFit="cover" quality={100} />
+			</section>
+		</div>
+	);
 };
 
 export default Home;
