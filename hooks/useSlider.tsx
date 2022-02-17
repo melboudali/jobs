034dfcb12cfabdc3b1d19de32eb86e31779bff22
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-02-17 22:42:31 +0000

    added useSlider hook

---
+++ hooks/useSlider.tsx
@@ -0,0 +1,54 @@
+import { RefObject, useRef, useState } from "react";
+
+interface Props {
+	sliderRef: RefObject<HTMLDivElement>;
+	containerRef: RefObject<HTMLDivElement>;
+}
+
+const useSlider = ({ containerRef, sliderRef }: Props) => {
+	const currentWidth = useRef(0);
+	const [translateValue, setTranslateValue] = useState(0);
+
+	const [slideButtons, setSlideButtons] = useState({
+		back: false,
+		next: true
+	});
+
+	const slideNext = () => {
+		if (!currentWidth.current) currentWidth.current = containerRef.current!.clientWidth;
+		if (currentWidth.current < sliderRef.current!.clientWidth && sliderRef.current!.clientWidth - currentWidth.current > 273) {
+			currentWidth.current! += 273;
+			setTranslateValue(currentWidth.current - containerRef.current!.clientWidth);
+			{
+				setSlideButtons({ back: true, next: true });
+			}
+			console.log(currentWidth.current);
+		} else {
+			sliderRef.current!.clientWidth - currentWidth.current > 0 ? (currentWidth.current += 273) : null;
+			setTranslateValue(sliderRef.current!.clientWidth - containerRef.current!.clientWidth);
+			{
+				slideButtons.next && setSlideButtons({ ...slideButtons, next: false });
+			}
+			console.log(sliderRef.current!.clientWidth - currentWidth.current);
+		}
+	};
+
+	const slideBack = () => {
+		if (currentWidth.current > containerRef.current!.clientWidth) {
+			currentWidth.current -= 273;
+			setTranslateValue(currentWidth.current - containerRef.current!.clientWidth);
+			{
+				!slideButtons.next && slideButtons.back && setSlideButtons({ back: true, next: true });
+			}
+		}
+		if (currentWidth.current === containerRef.current!.clientWidth) {
+			currentWidth.current = containerRef.current!.clientWidth;
+			{
+				slideButtons.back && setSlideButtons({ ...slideButtons, back: false });
+			}
+		}
+	};
+	return { translateValue, slideButtons, slideBack, slideNext };
+};
+
+export default useSlider;
