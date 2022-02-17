ead90f2bea46ad5f4b510dc7c1ca3170f54a8215
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-02-17 21:08:24 +0000

    added bottons functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,75 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import { useRef, useState } from "react";
 import JobCard from "../components/jobCard";
 import Title from "../components/title";
 import styles from "./index.module.scss";
 
 const Home: NextPage = () => {
+	const data = [
+		{ id: 0 },
+		{ id: 1 },
+		{ id: 2 },
+		{ id: 3 },
+		{ id: 4 },
+		{ id: 5 },
+		{ id: 6 },
+		{ id: 7 },
+		{ id: 8 },
+		{ id: 9 },
+		{ id: 10 },
+		{ id: 11 }
+	];
+	const sliderRef = useRef<HTMLDivElement>(null);
+	const containerRef = useRef<HTMLDivElement>(null);
+	const slideBy = useRef(1);
+	const currentIndex = useRef(0);
+	const leftItems = useRef(data.length - 4); //TODO: change the value later (items array length - hwo many items we see on screen)
+	const currentWidth = useRef(0);
+	const [translateValue, setTranslateValue] = useState(0);
+
+	const [slideButtons, setSlideButtons] = useState({
+		back: false,
+		next: true
+	});
+
+	const slideRight = () => {
+		if (!currentWidth.current) currentWidth.current = containerRef.current!.clientWidth;
+		if (currentWidth.current < sliderRef.current!.clientWidth && sliderRef.current!.clientWidth - currentWidth.current > 273) {
+			currentWidth.current! += 273;
+			setTranslateValue(currentWidth.current - containerRef.current!.clientWidth);
+			setSlideButtons({ ...slideButtons, next: true });
+		} else {
+			currentWidth.current! -= sliderRef.current!.clientWidth - currentWidth.current;
+			setTranslateValue(sliderRef.current!.clientWidth - containerRef.current!.clientWidth);
+			setSlideButtons({ ...slideButtons, next: false });
+		}
+	};
+
+	const slideLeft = () => {
+		// if (!currentWidth.current) currentWidth.current = containerRef.current!.clientWidth!;
+		if (currentWidth.current > 273) {
+			currentWidth.current -= 273;
+			setTranslateValue(currentWidth.current - containerRef.current!.clientWidth);
+			{
+				!slideButtons.back && setSlideButtons({ ...slideButtons, back: true });
+			}
+		}
+		// if (currentIndex.current > 0) {
+		// 	currentIndex.current--;
+		// 	leftItems.current++;ç
+		// 	setTranslateValue(currentIndex.current * 273);
+		// 	setSlideButtons({ ...slideButtons, next: true });
+		// 	console.log({ index: currentIndex.current, left: leftItems.current });
+		// } else {
+		// 	currentIndex.current = 0;
+		// 	leftItems.current = data.length - 4;
+		// 	setTranslateValue(0);
+		// 	setSlideButtons({ ...slideButtons, back: false });
+		// 	console.log({ index: currentIndex.current, left: leftItems.current });
+		// }
+	};
 	return (
 		<>
 			<Head>
@@ -25,22 +89,22 @@ const Home: NextPage = () => {
 				<div className={styles["title-wrapper"]}>
 					<Title>jobs hiring Now</Title>
 					<div className={styles.buttons}>
-						<button type="button" onClick={() => console.log("button clicked")} aria-label="back">
+						<button type="button" onClick={slideLeft} aria-label="back">
 							<svg width="7" height="10" viewBox="0 0 7 10" fill="none" xmlns="http://www.w3.org/2000/svg">
 								<path d="M6 1L2 5L6 9" stroke="#2B2E32" strokeWidth="2" strokeLinecap="round" />
 							</svg>
 						</button>
-						<button type="button" onClick={() => console.log("button clicked")} aria-label="next">
+						<button type="button" onClick={slideRight} aria-label="next">
 							<svg width="7" height="10" viewBox="0 0 7 10" fill="none" xmlns="http://www.w3.org/2000/svg">
 								<path d="M1 1L5 5L1 9" stroke="#2B2E32" strokeWidth="2" strokeLinecap="round" />
 							</svg>
 						</button>
 					</div>
 				</div>
-				<div className={styles["cards-slider"]}>
-					<div className={styles.cards}>
-						{[1, 2, 3, 4, 5, 6, 7, 8].map((el, idx) => (
-							<JobCard key={idx}>{el}</JobCard>
+				<div className={styles["cards-slider"]} ref={containerRef}>
+					<div style={{ transform: `translateX(-${translateValue}px)` }} className={styles.cards} ref={sliderRef}>
+						{data.map(({ id }) => (
+							<JobCard key={id}></JobCard>
 						))}
 					</div>
 				</div>
