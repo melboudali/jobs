f894cf70f47575c88d4705331bf121f12652b72e
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-02-19 17:48:34 +0000

    added images links

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const path = require("path");
 module.exports = {
 	reactStrictMode: true,
 	images: {
-		domains: ["dexibell.com"]
+		domains: ["inkbotdesign.com", "1000logos.net"]
 	},
 	sassOptions: {
 		includePaths: [path.join(__dirname, "styles")]
