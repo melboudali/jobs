4494ff1688dbef2d88247064fccfda13b0c0b179
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-02-20 13:30:38 +0000

    added icons component

---
+++ components/icons.tsx
@@ -0,0 +1,9 @@
+import styles from "./icons.module.scss";
+
+interface Props {}
+
+const Icons = ({}: Props) => {
+	return <div className={styles.icons}>This is icons Component/Page</div>;
+};
+
+export default Icons;
