fcd10527009b43392fec1846d6e0a5a4fa62975e
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-02-20 13:19:25 +0000

    added hook

--- hooks/useAppearOnScroll.tsx
+++ hooks/useAppearOnScroll.tsx
@@ -5,7 +5,7 @@ const useAppearOnScroll = (ref: React.RefObject<HTMLDivElement>) => {
 
 	useEffect(() => {
 		const easeContent = () => {
-			if (ref.current) ref.current.getBoundingClientRect().top < globalThis.innerHeight && setAppear(true);
+			if (ref.current && ref.current.getBoundingClientRect().top < globalThis.innerHeight) setAppear(true);
 		};
 		globalThis.addEventListener("scroll", easeContent);
 		return () => globalThis.removeEventListener("scroll", easeContent);
