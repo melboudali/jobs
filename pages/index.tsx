4e0c56d910929adc4fbc42b6cb6c7486905eac16
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-02-22 18:56:50 +0000

    added icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,108 +3,16 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import { useRef, useState } from "react";
-import Card from "../components/job-card";
-import Title from "../components/title";
+import Card from "../components/pages/home/job-card";
+import Title from "../components/pages/home/title";
 import styles from "./index.module.scss";
 import cls from "classnames";
 import useSlider from "../hooks/useSlider";
-import Stats from "../components/stats";
-import Icons from "../components/icons";
+import Stats from "../components/pages/home/stats";
+import Icons from "../components/pages/home/icons";
+import data from "../data/jobs.json";
 
 const Home: NextPage = () => {
-	const data = [
-		{
-			id: 1,
-			logo: "https://1000logos.net/wp-content/themes/redwaves-lite/pic/facebook-logo-small.png",
-			name: "facebook",
-			title: "Software Consultant",
-			price: "$77",
-			time: "full-time",
-			place: "80468 Claremont Parkway"
-		},
-		{
-			id: 2,
-			logo: "https://inkbotdesign.com/wp-content/uploads/2012/09/Twitter-Logo-Design.png",
-			name: "twitter",
-			title: "Editor",
-			price: "$43",
-			time: "part-time",
-			place: "04 Crest Line Terrace"
-		},
-		{
-			id: 3,
-			logo: "https://inkbotdesign.com/wp-content/uploads/2012/09/Apple-Logo-Design.png",
-			name: "apple",
-			title: "Web Designer",
-			price: "$55",
-			time: "full-time",
-			place: "8 Fordem Road"
-		},
-		{
-			id: 4,
-			logo: "https://inkbotdesign.com/wp-content/uploads/2012/09/Shell-Logo-Design.png",
-			name: "Shell",
-			title: "Nurse Practicioner",
-			price: "$34",
-			time: "full-time",
-			place: "98319 Myrtle Place"
-		},
-		{
-			id: 5,
-			logo: "https://inkbotdesign.com/wp-content/uploads/2012/09/Volkswagen-Logo-Design.png",
-			name: "Volkswagen",
-			title: "Senior Financial Analyst",
-			price: "$65",
-			time: "part-time",
-			place: "046 Lakeland Junction"
-		},
-		{
-			id: 6,
-			logo: "https://inkbotdesign.com/wp-content/uploads/2012/09/Pepsi-Logo-Design.png",
-			name: "Pepsi",
-			title: "Graphic Designer",
-			price: "$73",
-			time: "part-time",
-			place: "240 Melody Place"
-		},
-		{
-			id: 7,
-			logo: "https://inkbotdesign.com/wp-content/uploads/2012/09/BMW-Logo-Design.jpg",
-			name: "BMW",
-			title: "Sales Associate",
-			price: "$49",
-			time: "remote",
-			place: "90 Colorado Plaza"
-		},
-		{
-			id: 8,
-			logo: "https://inkbotdesign.com/wp-content/uploads/2012/09/Dell-Logo-Design.png",
-			name: "Dell",
-			title: "front end developer",
-			price: "$59",
-			time: "part-time",
-			place: "7 Elgar Place"
-		},
-		{
-			id: 9,
-			logo: "https://inkbotdesign.com/wp-content/uploads/2012/09/Nestle-Logo-Design.png",
-			name: "Nestle",
-			title: "full ftack developer",
-			price: "$95",
-			time: "remote",
-			place: "66234 Forest Lane"
-		},
-		{
-			id: 10,
-			logo: "https://inkbotdesign.com/wp-content/uploads/2012/09/Mitsubishi-Logo-Design.png",
-			name: "Mitsubishi",
-			title: "electrical engineer",
-			price: "$59",
-			time: "full-time",
-			place: "358 Lukken Circle"
-		}
-	];
-
 	const containerRef = useRef<HTMLDivElement>(null);
 	const sliderRef = useRef<HTMLDivElement>(null);
 	const { translateValue, slideButtons, slideBack, slideNext } = useSlider({ containerRef, sliderRef });
