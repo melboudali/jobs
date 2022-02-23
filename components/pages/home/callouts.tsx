74a689114d46052ea896d276fbed0bbe35db2426
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-02-23 23:33:04 +0000

    added Callout

--- components/pages/home/callouts.tsx
+++ components/pages/home/callouts.tsx
@@ -2,22 +2,39 @@ import Image from "next/image";
 import { FC } from "react";
 import styles from "./callouts.module.scss";
 import data from "../../../data/callouts.json";
+import cls from "classnames";
 
 interface Props {}
 
 const Callouts = ({}: Props) => {
 	return (
-		<div className={styles.root}>
-			<Callout direction="left" />
-			<Callout direction="left" />
-		</div>
+		<>
+			{data.map(({ id, image, title, description, buttonText, direction }) => (
+				<Callout key={id} image={image} direction={direction} description={description} />
+			))}
+		</>
 	);
 };
 
-const Callout: FC<{ direction: "right" | "left" }> = ({ direction }) => {
+const Callout: FC<{ direction: string; image: string; description: string }> = ({ direction, image, description }) => {
+	const callout__image = cls(styles.callout__image, {
+		[styles["callout__image-right"]]: direction === "right",
+		[styles["callout__image-left"]]: direction === "left"
+	});
+
+	const callout__details = cls(styles.callout__details, {
+		[styles["callout__details-right"]]: direction === "right",
+		[styles["callout__details-left"]]: direction === "left"
+	});
+
 	return (
-		<div>
-			<Image src={data[0].image} alt="hero image" width={399} height={399} objectFit="cover" quality={100}></Image>
+		<div className={styles.root}>
+			<div className={callout__image}>
+				<Image src={image} alt="hero image" layout="fill" objectFit="cover" quality={100} className={styles.callout__image__img} />
+			</div>
+			<div className={callout__details}>
+				<p>{description}</p>
+			</div>
 		</div>
 	);
 };
