6027d47b90a24a36b67b4ec032487f9a80807e73
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-02-24 21:36:46 +0000

    added icons id

--- components/pages/home/icons.tsx
+++ components/pages/home/icons.tsx
@@ -4,8 +4,8 @@ import data from "../../../data/icons.json";
 const Icons = () => (
 	<div className={styles.wrapper}>
 		<div className={styles.svgs}>
-			{data.map(({ name, path }) => (
-				<div key={name} className={styles.svgs__svg}>
+			{data.map(({ id, name, path }) => (
+				<div key={id} className={styles.svgs__svg}>
 					<span>{name}</span>
 					<svg width="70" height="70" viewBox="0 0 70 70" fill="none" xmlns="http://www.w3.org/2000/svg">
 						<path d={path} />
