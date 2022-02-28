3cda490f93ccef80701e8153bc41132d5210bfa1
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-02-28 17:44:31 +0000

    split components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
@@ -15,101 +15,116 @@ import Callouts from "../components/pages/home/callouts";
 import testimonials from "../data/testimonials.json";
 import { flexThis } from "../utils";
 import Posts from "../components/pages/home/posts";
+import Subscribe from "../components/pages/home/subscribe";
 
-const Home: NextPage = () => {
-	console.log("rendered");
+const Home: NextPage = () => (
+	<>
+		<Head>
+			<title>Jobs</title>
+		</Head>
+		<section className={styles.hero}>
+			<div className={styles.hero__wrapper}>
+				<h1 className={styles.hero__title}>We&apos;re here to make hiring a little easier. Let&apos;s give it a try.</h1>
+				<Link href="/">
+					<a className={styles.hero__link}>post a job for free</a>
+				</Link>
+			</div>
+			<Image src="/hero.jpg" alt="hero image" layout="fill" objectFit="cover" quality={100} priority />
+		</section>
+		<section className={styles.container}>
+			<Jobs />
+		</section>
+		<section className={styles.container}>
+			<Callouts />
+		</section>
+		<section className={styles.container}>
+			<Stats />
+		</section>
+		<section className={styles.container}>
+			<Title>testimonials by people like you</Title>
+			<Testimonials />
+		</section>
+		<section className={styles.container}>
+			<div className={styles["title-wrapper"]}>
+				<Title>From the blog</Title>
+				<Link href="/blog">
+					<a className={styles["more-posts__button"]}>more posts</a>
+				</Link>
+			</div>
+			<Posts />
+		</section>
+		<section className={styles.container}>
+			<Title>we are trusted by</Title>
+			<Icons />
+		</section>
+		<section className={styles.container}>
+			<Subscribe />
+		</section>
+	</>
+);
+
+const Jobs = () => {
 	const containerRef = useRef<HTMLDivElement>(null);
 	const sliderRef = useRef<HTMLDivElement>(null);
 	const { translateValue, slideButtons, slideBack, slideNext } = useSlider({ containerRef, sliderRef });
-	const [more, setMore] = useState(false);
 
-	const nextBtnClass = cls(styles.button, {
+	const next_button_styles = cls(styles.button, {
 		[styles["button-disabled"]]: !slideButtons.next
 	});
 
-	const backBtnClass = cls(styles.button, {
-		[styles["button-disabled"]]: !slideButtons.back
+	const prev_button_styles = cls(styles.button, {
+		[styles["button-disabled"]]: !slideButtons.prev
 	});
-
-	const testimonialsClass = cls(styles.testimonials, { [styles["testimonials__showAll"]]: more });
 
 	return (
 		<>
-			<Head>
-				<title>Jobs</title>
-			</Head>
-			<section className={styles.hero}>
-				<div className={styles.hero__wrapper}>
-					<h1 className={styles.hero__title}>We&apos;re here to make hiring a little easier. Let&apos;s give it a try.</h1>
-					<Link href="/">
-						<a className={styles.hero__link}>post a job for free</a>
-					</Link>
-				</div>
-				<Image src="/hero.jpg" alt="hero image" layout="fill" objectFit="cover" quality={100} priority />
-			</section>
-			<section className={styles.container}>
-				<div className={styles["title-wrapper"]}>
-					<Title>jobs hiring now</Title>
-					<div className={styles.buttons}>
-						<button className={backBtnClass} type="button" onClick={slideBack} aria-label="back">
-							<svg width="7" height="10" viewBox="0 0 7 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-								<path d="M6 1L2 5L6 9" strokeWidth="2" strokeLinecap="round" />
-							</svg>
-						</button>
-						<button className={nextBtnClass} type="button" onClick={slideNext} aria-label="next">
-							<svg width="7" height="10" viewBox="0 0 7 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-								<path d="M1 1L5 5L1 9" strokeWidth="2" strokeLinecap="round" />
-							</svg>
-						</button>
-					</div>
-				</div>
-				<div className={styles["cards-slider"]} ref={containerRef}>
-					<div style={{ transform: `translateX(-${translateValue}px)` }} className={styles.cards} ref={sliderRef}>
-						{data.map(({ id, ...others }) => (
-							<Card key={id} data={others} />
-						))}
-					</div>
-				</div>
-			</section>
-			<section className={styles.container}>
-				<Callouts />
-			</section>
-			<section className={styles.container}>
-				<Stats />
-			</section>
-			<section className={styles.container}>
-				<Title>testimonials by people like you</Title>
-				<div className={testimonialsClass}>
-					{flexThis(testimonials).map((testimonials, idx) => (
-						<div key={idx} className={styles.testimonials__column}>
-							{testimonials.map(({ id, image, name, job, message }) => (
-								<Card key={id} data={{ logo: image, name: job, title: name }} testimonial>
-									{message}
-								</Card>
-							))}
-						</div>
-					))}
-
-					<div className={styles.testimonials__more__wrapper} />
-					<button className={styles.testimonials__more__button} type="button" onClick={() => setMore(!more)}>
-						show more
+			<div className={styles["title-wrapper"]}>
+				<Title>jobs hiring now</Title>
+				<div className={styles.buttons}>
+					<button className={prev_button_styles} type="button" onClick={slideBack} aria-label="back">
+						<svg width="7" height="10" viewBox="0 0 7 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path d="M6 1L2 5L6 9" strokeWidth="2" strokeLinecap="round" />
+						</svg>
+					</button>
+					<button className={next_button_styles} type="button" onClick={slideNext} aria-label="next">
+						<svg width="7" height="10" viewBox="0 0 7 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path d="M1 1L5 5L1 9" strokeWidth="2" strokeLinecap="round" />
+						</svg>
 					</button>
 				</div>
-			</section>
-			<section className={styles.container}>
-				<div className={styles["title-wrapper"]}>
-					<Title>From the blog</Title>
-					<Link href="/blog">
-						<a className={styles["more-posts__button"]}>more posts</a>
-					</Link>
+			</div>
+			<div className={styles["cards-slider"]} ref={containerRef}>
+				<div style={{ transform: `translateX(-${translateValue}px)` }} className={styles.cards} ref={sliderRef}>
+					{data.map(({ id, ...others }) => (
+						<Card key={id} data={others} />
+					))}
 				</div>
-				<Posts />
-			</section>
-			<section className={styles.container}>
-				<Title>we are trusted by</Title>
-				<Icons />
-			</section>
+			</div>
 		</>
+	);
+};
+
+const Testimonials = () => {
+	const [more, setMore] = useState(false);
+
+	const testimonials_styles = cls(styles.testimonials, { [styles["testimonials__showAll"]]: more });
+
+	return (
+		<div className={testimonials_styles}>
+			{flexThis(testimonials).map((testimonials, idx) => (
+				<div key={idx} className={styles.testimonials__column}>
+					{testimonials.map(({ id, image, name, job, message }) => (
+						<Card key={id} data={{ logo: image, name: job, title: name }} testimonial>
+							{message}
+						</Card>
+					))}
+				</div>
+			))}
+			<div className={styles.testimonials__more__wrapper} />
+			<button className={styles.testimonials__more__button} type="button" onClick={() => setMore(!more)}>
+				show more
+			</button>
+		</div>
 	);
 };
 
