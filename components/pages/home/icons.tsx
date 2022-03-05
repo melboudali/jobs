9191c55ae18eef262463b4d358c507eb042707b3
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-03-05 17:46:29 +0000

    added title

--- components/pages/home/icons.tsx
+++ components/pages/home/icons.tsx
@@ -1,19 +1,23 @@
-import styles from "./icons.module.scss";
+import Title from "./title";
 import data from "../../../data/icons.json";
+import styles from "./icons.module.scss";
 
 const Icons = () => (
-	<div className={styles.wrapper}>
-		<div className={styles.svgs}>
-			{data.map(({ id, name, path }) => (
-				<div key={id} className={styles.svgs__svg}>
-					<span>{name}</span>
-					<svg width="70" height="70" viewBox="0 0 70 70" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<path d={path} />
-					</svg>
-				</div>
-			))}
+	<>
+		<Title>trusted by</Title>
+		<div className={styles.wrapper}>
+			<div className={styles.svgs}>
+				{data.map(({ id, name, path }) => (
+					<div key={id} className={styles.svgs__svg}>
+						<span>{name}</span>
+						<svg width="70" height="70" viewBox="0 0 70 70" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path d={path} />
+						</svg>
+					</div>
+				))}
+			</div>
 		</div>
-	</div>
+	</>
 );
 
 export default Icons;
