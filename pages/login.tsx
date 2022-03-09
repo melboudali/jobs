6dbfcdad0dcbd92ae5b60d8114a9d518d3c8701c
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-03-09 21:22:14 +0000

    added login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,34 +1,52 @@
-import React, { ChangeEvent, ReactElement, useState } from "react";
-import styles from "../components/pages/login/index.module.scss";
-import PropTypes from "prop-types";
+import React, { FormEvent, ReactElement, useRef, useState } from "react";
+import styles from "./login.module.scss";
 import SignLayout from "../layouts/signLayout";
-import Logo from "../components/common/navbar/logo";
+import LargeLogo from "../components/common/loginSignup/largeLogo";
+import PropTypes from "prop-types";
+import Input from "../components/common/loginSignup/input";
+import useForm from "../hooks/useForm";
+import ReCAPTCHA from "react-google-recaptcha";
 
 const Login = () => {
-	const [email, setEmail] = useState("");
-	const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-		setEmail(e.target.value);
+	const recaptchaRef = useRef<ReCAPTCHA>(null);
+	const { values, updateValue, clearValues } = useForm({ email: "", password: "" });
+	const [recaptcha, setRecaptcha] = useState("");
+
+	const onSubmit = (e: FormEvent) => {
+		e.preventDefault();
+		console.log(recaptcha);
+		clearValues();
 	};
 
 	return (
 		<div>
-			<div className={styles.logo}>
-				<Logo large />
-				<p className={styles.logo__name}>Sign in to jobs</p>
-			</div>
-			<form className={styles.form}>
-				<div className={styles.input__wrapper}>
-					<label htmlFor="email" className={styles.label}>
-						email address
-					</label>
-					<input type="email" name="email" id="email" value={email} onChange={onInputChange} className={styles.input} />
-				</div>
-				<div className={styles.input__wrapper}>
-					<label htmlFor="password" className={styles.label}>
-						password
-					</label>
-					<input type="password" name="password" id="password" value={email} onChange={onInputChange} className={styles.input} />
-				</div>
+			<LargeLogo />
+			<form className={styles.form} onSubmit={onSubmit}>
+				<Input
+					id="email"
+					name="email"
+					label="email address"
+					type="email"
+					placeHolder="Enter your email address"
+					value={values["email"]}
+					onChange={updateValue}
+				/>
+				<Input id="password" name="password" label="password" type="password" value={values["password"]} onChange={updateValue} />
+
+				{process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY && (
+					<ReCAPTCHA
+						className={styles.recaptcha}
+						ref={recaptchaRef}
+						sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
+						onChange={(value: string | null) => {
+							if (!value) {
+								return null;
+							}
+							setRecaptcha(value);
+						}}
+					/>
+				)}
+				<input className={styles.submit} type="submit" value="sign in" />
 			</form>
 		</div>
 	);
