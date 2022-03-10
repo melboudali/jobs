58b7cbf951a75d48a0f921e2eca5a01c4ddabc70
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-03-10 18:12:18 +0000

    Updated

--- pages/password_reset.tsx
+++ pages/password_reset.tsx
@@ -1,5 +1,4 @@
-import type { NextPage, GetStaticProps, GetStaticPaths, GetServerSideProps, MyNextPage } from "next";
-import PropTypes from "prop-types";
+import type { MyNextPage } from "next";
 import { ReactElement } from "react";
 
 interface Props {}
@@ -7,29 +6,6 @@ interface Props {}
 const PasswordReset: MyNextPage<Props> = props => {
 	return <div>This is PageName Component/Page</div>;
 };
-
-// export const getStaticPaths: GetStaticPaths = async () => {
-//     const res = await fetch('');
-//     const posts = await res.json();
-//     const paths = posts.map((post: any) => ({
-//         params: { id: post.id },
-//     }))
-//     return { paths, fallback: false }
-// }
-
-// export const getStaticProps: GetStaticProps = async context => {
-//     const res = await fetch(``)
-//     const post = await res.json()
-//     return { props: { post } }
-// }
-
-// export const getServerSideProps: GetServerSideProps = async context => {
-//     const res = await fetch(``);
-//     const data = await res.json()
-//     return { props:{data}}
-// }
-
-PasswordReset.propTypes = {};
 
 PasswordReset.getLayout = (page: ReactElement) => page;
 
