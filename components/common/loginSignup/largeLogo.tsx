5c8e882e77919990ac30e7a991fdadf4a7a9e4d8
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-03-11 12:40:47 +0000

    added variants

--- components/common/loginSignup/largeLogo.tsx
+++ components/common/loginSignup/largeLogo.tsx
@@ -1,11 +1,21 @@
 import Logo from "../Navbar/Logo";
 import styles from "./LargeLogo.module.scss";
 
-const LargeLogo = () => (
+interface Props {
+	variant: "login" | "signup" | "password-rest";
+}
+
+const LargeLogo = ({ variant }: Props) => (
 	<div className={styles.logo}>
 		<Logo large />
-		<p className={styles.logo__name}>Sign in to jobs</p>
+		<p className={styles.logo__name}>
+			{variant === "login" ? "Sign in to jobs" : variant === "signup" ? "Sign up to jobs" : "Reset your password"}
+		</p>
 	</div>
 );
+
+LargeLogo.defaultProps = {
+	isSignUp: false
+};
 
 export default LargeLogo;
