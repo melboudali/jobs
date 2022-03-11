62b5c10833e613fe08a3d5fdae8aad12ee536884
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-03-11 17:30:10 +0000

    tabs size reduced

--- types/next.d.ts
+++ types/next.d.ts
@@ -2,14 +2,14 @@ import type { NextComponentType, NextPageContext, NextLayoutComponentType } from
 import type { AppProps } from "next/app";
 
 declare module "next" {
-	type MyNextPage<P = {}, IP = P> = React.ComponentType<P> & {
-		getInitialProps?(context: NextPageContext): IP | Promise<IP>;
-		getLayout?: (page: ReactNode) => ReactNode;
-	};
+   type MyNextPage<P = {}, IP = P> = React.ComponentType<P> & {
+      getInitialProps?(context: NextPageContext): IP | Promise<IP>;
+      getLayout?: (page: ReactNode) => ReactNode;
+   };
 }
 
 declare module "next/app" {
-	type AppPropsWithLayout = AppProps & {
-		Component: MyNextPage;
-	};
+   type AppPropsWithLayout = AppProps & {
+      Component: MyNextPage;
+   };
 }
