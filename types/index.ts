1fec5ba1a3cfe93f35403eed99fd0c1d3074695c
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-03-11 12:45:00 +0000

    added useFormValues interface

--- types/index.ts
+++ types/index.ts
@@ -19,3 +19,12 @@ export interface Post {
 }
 
 export type Posts = Post[];
+
+// Hooks
+export interface useFormValues {
+	firstName?: string;
+	lastName?: string;
+	email: string;
+	password: string;
+	resetPassword?: string;
+}
