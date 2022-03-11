2df158f02809c4975aa6f41a430bbd0eccaafa44
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-03-11 13:30:44 +0000

    Removed

--- components/pages/home/title.tsx
+++
@@ -1,6 +0,0 @@
-import { FC } from "react";
-import styles from "../../../pages/index.module.scss";
-
-const Title: FC = ({ children }) => <h2 className={styles.title}>{children}</h2>;
-
-export default Title;
