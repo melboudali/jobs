1d98304e623e4b562477b34e6b0cf6c151413860
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-03-11 17:29:34 +0000

    added YupError interface

--- types/index.ts
+++ types/index.ts
@@ -1,30 +1,35 @@
 export interface testimonial {
-	id: number;
-	image: string;
-	name: string;
-	job: string;
-	message: string;
+   id: number;
+   image: string;
+   name: string;
+   job: string;
+   message: string;
 }
 
 export type testimonials = testimonial[];
 
 export interface Post {
-	id: string;
-	title: string;
-	cover: string;
-	seo_description: string;
-	summary: string;
-	contentHtml: string;
-	date: string;
+   id: string;
+   title: string;
+   cover: string;
+   seo_description: string;
+   summary: string;
+   contentHtml: string;
+   date: string;
 }
 
 export type Posts = Post[];
 
 // Hooks
 export interface useFormValues {
-	firstName?: string;
-	lastName?: string;
-	email: string;
-	password: string;
-	resetPassword?: string;
+   firstName?: string;
+   lastName?: string;
+   email: string;
+   password: string;
+   resetPassword?: string;
+}
+
+// Yup
+export interface YupError {
+   message: { field: string; message: string };
 }
