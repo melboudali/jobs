e6cb527dc97ff94d61f180ab930d4378b99fc318
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-03-11 12:44:10 +0000

    added signup page

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,16 +1,21 @@
 import type { MyNextPage } from "next";
 import PropTypes from "prop-types";
 import { ReactElement } from "react";
+import Form from "../components/common/LoginSignup/Form";
+import LargeLogo from "../components/common/LoginSignup/LargeLogo";
 import SignLayout from "../layouts/SignLayout";
 
-const Signup: MyNextPage = () => {
-	return <div>This is PageName Component/Page</div>;
-};
+const Signup: MyNextPage = () => (
+    <>
+        <LargeLogo variant="signup" />
+        <Form variant="signup" />
+    </>
+);
 
 Signup.getLayout = (page: ReactElement) => (
-	<SignLayout question="Already have an account" suggestion="Log in" to="/login">
-		{page}
-	</SignLayout>
+    <SignLayout question="Already have an account" suggestion="Log in" to="/login">
+        {page}
+    </SignLayout>
 );
 
 export default Signup;
