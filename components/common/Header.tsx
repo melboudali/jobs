7cc674fafe6fa67c46ee2e99c385641842e9e599
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-03-12 23:56:06 +0000

    fixed typo

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -2,9 +2,9 @@ import Navar from "./Navbar";
 import styles from "./Header.module.scss";
 
 const Header = () => (
-	<header className={styles.header}>
-		<Navar></Navar>
-	</header>
+   <header className={styles.header}>
+      <Navar />
+   </header>
 );
 
 export default Header;
