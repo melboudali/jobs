6ff691d774b89d8df62c11edb3b0c43a68cdee0f
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-03-14 17:32:34 +0000

    removed customLink

--- components/common/Navbar/index.tsx
+++ components/common/Navbar/index.tsx
@@ -1,62 +1,34 @@
+import { Dispatch, SetStateAction, useState } from "react";
 import Link from "next/link";
 import Logo from "./Logo";
+import cls from "classnames";
 import styles from "./Navbar.module.scss";
+import Links from "./Links";
+import data from "../../../data/navbarLinks.json";
 
 interface Props {
-	name: string;
-	href: string;
-	last: boolean;
+   showHamburgerMenu: boolean;
+   toggle: () => void;
 }
 
-const Navbar = () => {
-	const links = [
-		{ name: "how it works", href: "/how-it-works" },
-		{ name: "pricing", href: "/pricing" },
-		{ name: "blog", href: "/blog" },
-		{ name: "about", href: "/about" },
-		{ name: "support", href: "/support" },
-		{ name: "contact", href: "/contact" },
-		{ name: "Start Free", href: "/login" }
-	];
+const Navbar = ({ toggle, showHamburgerMenu }: Props) => {
+   const svgStyles = cls(styles.svg, { [styles["svg--active"]]: showHamburgerMenu });
 
-	return (
-		<div className={styles.navbar}>
-			<Logo />
-			<nav className={styles.navbar__nav}>
-				<ul>
-					{links.map(({ name, href }, idx) => (
-						<li key={idx} className={styles.navbar__nav__li}>
-							<CustomLink name={name} href={href} last={idx === links.length - 1} />
-						</li>
-					))}
-				</ul>
-			</nav>
-			<button className={styles.navbar__hamburger} aria-label="menu" onClick={() => console.log("menu clicked")}>
-				<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<path d="M2 5L22 5" strokeWidth="3" strokeLinecap="round" />
-					<path d="M8 12L22 12" strokeWidth="3" strokeLinecap="round" />
-					<path d="M14 19L22 19" strokeWidth="3" strokeLinecap="round" />
-				</svg>
-			</button>
-		</div>
-	);
+   return (
+      <div className={styles.navbar}>
+         <Logo />
+         <nav className={styles.navbar__nav}>
+            <Links variant="desktop" data={data} />
+         </nav>
+         <button className={styles.navbar__hamburger} aria-label="menu" onClick={toggle}>
+            <svg className={svgStyles} width="40" height="40" viewBox="0 0 100 100">
+               <path d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058" />
+               <path d="M 20,50 H 80" />
+               <path d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942" />
+            </svg>
+         </button>
+      </div>
+   );
 };
-
-const CustomLink = ({ name, href, last }: Props) => (
-	<Link href={href}>
-		<a>
-			{last ? (
-				<>
-					<span>{name}</span>
-					<svg width="7" height="10" viewBox="0 0 7 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<path d="M1 1L5 5L1 9" strokeWidth="2" strokeLinecap="round" />
-					</svg>
-				</>
-			) : (
-				name
-			)}
-		</a>
-	</Link>
-);
 
 export default Navbar;
