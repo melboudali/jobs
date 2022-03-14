aa585d036563929f13489c9f878f792eab8f284e
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-03-14 17:04:31 +0000

    added hamburger menu

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import Navar from "./Navbar";
+import cls from "classnames";
 import styles from "./Header.module.scss";
+import Links from "./Navbar/Links";
+import data from "../../data/navbarLinks.json";
 
-const Header = () => (
-   <header className={styles.header}>
-      <Navar />
-   </header>
-);
+const Header = () => {
+   const [showHamburgerMenu, setShowHamburgerMenu] = useState(false);
+
+   const toggle = () => {
+      document.documentElement.style.overflowY = showHamburgerMenu ? "visible" : "hidden";
+      setShowHamburgerMenu(!showHamburgerMenu);
+   };
+
+   const headerStyles = cls(styles.header, { [styles["header--open"]]: showHamburgerMenu });
+
+   return (
+      <header className={headerStyles}>
+         <Navar showHamburgerMenu={showHamburgerMenu} toggle={toggle} />
+         {showHamburgerMenu && <Links variant="mobile" data={data} showHamburgerMenu={showHamburgerMenu} />}
+      </header>
+   );
+};
 
 export default Header;
