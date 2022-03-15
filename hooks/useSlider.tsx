ed6be9b55a0c0e31da8b56785d8c61d55d69bcf3
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-03-15 23:58:40 +0000

    added type keyword

--- hooks/useSlider.tsx
+++ hooks/useSlider.tsx
@@ -1,52 +1,55 @@
-import { RefObject, useRef, useState } from "react";
+import { useRef, useState, type RefObject } from "react";
 
 interface Props {
-	sliderRef: RefObject<HTMLDivElement>;
-	containerRef: RefObject<HTMLDivElement>;
+   sliderRef: RefObject<HTMLDivElement>;
+   containerRef: RefObject<HTMLDivElement>;
 }
 
 const useSlider = ({ containerRef, sliderRef }: Props) => {
-	const currentWidth = useRef(0);
-	const [translateValue, setTranslateValue] = useState(0);
+   const currentWidth = useRef(0);
+   const [translateValue, setTranslateValue] = useState(0);
 
-	const [slideButtons, setSlideButtons] = useState({
-		prev: false,
-		next: true
-	});
+   const [slideButtons, setSlideButtons] = useState({
+      prev: false,
+      next: true,
+   });
 
-	const slideNext = () => {
-		if (!currentWidth.current) currentWidth.current = containerRef.current!.clientWidth;
-		if (currentWidth.current < sliderRef.current!.clientWidth && sliderRef.current!.clientWidth - currentWidth.current > 273) {
-			currentWidth.current! += 273;
-			setTranslateValue(currentWidth.current - containerRef.current!.clientWidth);
-			{
-				setSlideButtons({ prev: true, next: true });
-			}
-		} else {
-			sliderRef.current!.clientWidth - currentWidth.current > 0 ? (currentWidth.current += 273) : null;
-			setTranslateValue(sliderRef.current!.clientWidth - containerRef.current!.clientWidth);
-			{
-				slideButtons.next && setSlideButtons({ ...slideButtons, next: false });
-			}
-		}
-	};
+   const slideNext = () => {
+      if (!currentWidth.current) currentWidth.current = containerRef.current!.clientWidth;
+      if (
+         currentWidth.current < sliderRef.current!.clientWidth &&
+         sliderRef.current!.clientWidth - currentWidth.current > 273
+      ) {
+         currentWidth.current! += 273;
+         setTranslateValue(currentWidth.current - containerRef.current!.clientWidth);
+         {
+            setSlideButtons({ prev: true, next: true });
+         }
+      } else {
+         sliderRef.current!.clientWidth - currentWidth.current > 0 ? (currentWidth.current += 273) : null;
+         setTranslateValue(sliderRef.current!.clientWidth - containerRef.current!.clientWidth);
+         {
+            slideButtons.next && setSlideButtons({ ...slideButtons, next: false });
+         }
+      }
+   };
 
-	const slideBack = () => {
-		if (currentWidth.current > containerRef.current!.clientWidth) {
-			currentWidth.current -= 273;
-			setTranslateValue(currentWidth.current - containerRef.current!.clientWidth);
-			{
-				!slideButtons.next && slideButtons.prev && setSlideButtons({ prev: true, next: true });
-			}
-		}
-		if (currentWidth.current === containerRef.current!.clientWidth) {
-			currentWidth.current = containerRef.current!.clientWidth;
-			{
-				slideButtons.prev && setSlideButtons({ ...slideButtons, prev: false });
-			}
-		}
-	};
-	return { translateValue, slideButtons, slideBack, slideNext };
+   const slideBack = () => {
+      if (currentWidth.current > containerRef.current!.clientWidth) {
+         currentWidth.current -= 273;
+         setTranslateValue(currentWidth.current - containerRef.current!.clientWidth);
+         {
+            !slideButtons.next && slideButtons.prev && setSlideButtons({ prev: true, next: true });
+         }
+      }
+      if (currentWidth.current === containerRef.current!.clientWidth) {
+         currentWidth.current = containerRef.current!.clientWidth;
+         {
+            slideButtons.prev && setSlideButtons({ ...slideButtons, prev: false });
+         }
+      }
+   };
+   return { translateValue, slideButtons, slideBack, slideNext };
 };
 
 export default useSlider;
