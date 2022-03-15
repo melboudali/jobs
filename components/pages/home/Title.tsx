f36350dd707c6c0a74284e92865737cfb884f30f
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-03-15 23:51:52 +0000

    added type keyword

--- components/pages/home/Title.tsx
+++ components/pages/home/Title.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import type { FC } from "react";
 import styles from "../../../pages/index.module.scss";
 
 const Title: FC = ({ children }) => <h2 className={styles.title}>{children}</h2>;
