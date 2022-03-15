615eae5641bb07e977133bdd4eca26aca6613c8c
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-03-15 23:58:22 +0000

    added type keyword

--- hooks/useForm.tsx
+++ hooks/useForm.tsx
@@ -1,5 +1,5 @@
-import { ChangeEvent, useState } from "react";
-import { useFormValues } from "../types";
+import { useState, type ChangeEvent } from "react";
+import type { useFormValues } from "../types";
 
 const useForm = (defaults: useFormValues) => {
    const [values, setValues] = useState(defaults);
