dced3c3faf7536f90a98517746f0cf2aab4605c5
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-03-16 00:07:04 +0000

    added type keyword

--- utils/index.ts
+++ utils/index.ts
@@ -1,23 +1,9 @@
-import { testimonials } from "../types";
+import type { testimonials } from "../types";
 
 /** the flexThis take one array and returns three arrays for masonry layout.
  *
- * (testimonials[]) => [testimonials[], testimonials[], testimonials[]]
+ * (testimonials[]) => testimonials[][]
  */
-// export const flexThis = (arr: testimonials) => {
-//    const res: testimonials[] = [];
-//    let subRes: testimonials = [];
-//    for (let i = 0; i < arr.length; i++) {
-//       subRes.push(arr[i]);
-//       if (arr[i].id % 3 === 0 || i === arr.length - 1) {
-//          res.push(subRes);
-//          subRes = [];
-//       }
-//    }
-
-//    return res;
-// };
-
 export const flexThis = <T extends { id: number }>(arr: T[]): T[][] => {
    const res: T[][] = [];
    let subRes: T[] = [];
