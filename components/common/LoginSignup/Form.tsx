a06ba1e52c0a89450a559f70dcf7a218849ffeb4
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-03-16 21:47:31 +0000

    added form validation

--- components/common/LoginSignup/Form.tsx
+++ components/common/LoginSignup/Form.tsx
@@ -6,6 +6,7 @@ import Input from "./Input";
 import Recaptcha from "./Recaptcha";
 import SubmitButton from "./SubmitButton";
 import styles from "./Form.module.scss";
+import FormValidation from "../../../utils/FormValidation";
 
 interface Props {
    variant: "login" | "signup" | "password-rest";
@@ -17,49 +18,16 @@ interface InputsProps extends Props {
 }
 
 const Form = ({ variant }: Props) => {
-   const { values, updateValue, clearValues } = useForm({ email: "", password: "" });
+   const { values, updateValue, clearValues } = useForm({
+      email: "",
+   });
    const [recaptcha, setRecaptcha] = useState("");
 
    const onSubmit = async (e: FormEvent) => {
       e.preventDefault();
-      let schema = yup.object({
-         firstName: yup
-            .string()
-            .min(6, { field: "firstName", message: "First Name is too short - should be 6 chars minimum." })
-            .max(30, { field: "firstName", message: "First Name is too long - should be 30 chars maximum." })
-            .required({ field: "firstName", message: "First Name Required." }),
-         lastName: yup
-            .string()
-            .min(6, { field: "lastName", message: "Last Name is too short - should be 6 chars minimum." })
-            .max(30, { field: "lastName", message: "Last Name is too long - should be 30 chars maximum." })
-            .required({ field: "lastName", message: "Last Name Required." }),
-         email: yup
-            .string()
-            .email({ field: "email", message: "Invalid email address." })
-            .required({ field: "email", message: "Email Required." }),
-         password: yup
-            .string()
-            .min(6, { field: "password", message: "Password is too short - should be 6 chars minimum." })
-            .matches(/[a-zA-Z0-9]/, {
-               field: "password",
-               message: "Password can only contain Latin letters.",
-            }),
-         resetPassword: yup.string().email({ field: "resetPassword", message: "Invalid email address." }),
-         recaptcha: yup
-            .string()
-            .required({ field: "recaptcha", message: "Please verify that you are not a robot." }),
-      });
-
-      try {
-         const user = await schema.validate({ ...values, recaptcha });
-         console.log(user);
-         // clearValues();
-      } catch (err) {
-         const {
-            message: { field, message },
-         } = err as YupError;
-         console.log({ field, message });
-      }
+      const fromValidation = new FormValidation(variant, values, recaptcha);
+      const vl = await fromValidation.validate();
+      console.log(vl);
    };
 
    return (
@@ -91,7 +59,7 @@ const Inputs = ({ variant, values, updateValue }: InputsProps) => {
                label="password"
                type="password"
                isLoginPage
-               value={values.password}
+               value={values.password ?? ""}
                onChange={updateValue}
             />
          </>
@@ -130,7 +98,7 @@ const Inputs = ({ variant, values, updateValue }: InputsProps) => {
                name="password"
                label="password"
                type="password"
-               value={values.password}
+               value={values.password ?? ""}
                onChange={updateValue}
             />
          </>
@@ -140,11 +108,11 @@ const Inputs = ({ variant, values, updateValue }: InputsProps) => {
    return (
       <>
          <Input
-            id="resetPassword"
-            name="resetPassword"
-            label="Enter your user account's verified email address. "
+            id="email"
+            name="email"
+            label="Enter your user account's verified email address."
             type="email"
-            value={values.resetPassword ?? ""}
+            value={values.email}
             onChange={updateValue}
          />
       </>
