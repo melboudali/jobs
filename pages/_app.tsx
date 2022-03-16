57a820df8c4de1c596be5fe8f77077d3094626d6
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-03-16 00:03:00 +0000

    added type keyword

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
+import type { ReactNode } from "react";
 import type { AppPropsWithLayout } from "next/app";
-import { ReactNode } from "react";
 import "../styles/globals.scss";
 
 const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
-	const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
-	return getLayout(<Component {...pageProps} />);
+   const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
+   return getLayout(<Component {...pageProps} />);
 };
 
 export default MyApp;
