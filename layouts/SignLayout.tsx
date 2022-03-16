da8049f495ec9892c759d2bb600b169852d8ff47
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-03-16 00:02:27 +0000

    added type keyword

--- layouts/SignLayout.tsx
+++ layouts/SignLayout.tsx
@@ -2,16 +2,16 @@ import Footer from "../components/common/LoginSignup/Footer";
 import styles from "./Layouts.module.scss";
 
 interface Props {
-	question: string;
-	suggestion: string;
-	to: "/login" | "/signup";
+   question: string;
+   suggestion: string;
+   to: "/login" | "/signup";
 }
 
 const SignLayout: React.FC<Props> = ({ children, question, suggestion, to }) => (
-	<>
-		<main className={styles.signMain}>{children}</main>
-		<Footer question={question} suggestion={suggestion} to={to} />
-	</>
+   <>
+      <main className={styles.signMain}>{children}</main>
+      <Footer question={question} suggestion={suggestion} to={to} />
+   </>
 );
 
 export default SignLayout;
