f15dd2db05be47296641484cf1c2b7d16dc0668d
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-03-16 00:06:11 +0000

    added type keyword

--- pages/password_reset.tsx
+++ pages/password_reset.tsx
@@ -1,20 +1,20 @@
 import type { MyNextPage } from "next";
-import { ReactElement } from "react";
+import type { ReactElement } from "react";
 import Form from "../components/common/LoginSignup/Form";
 import LargeLogo from "../components/common/LoginSignup/LargeLogo";
 import SignLayout from "../layouts/SignLayout";
 
 const PasswordReset: MyNextPage = () => (
-  <>
-    <LargeLogo variant="password-rest" />
-    <Form variant="password-rest" />
-  </>
+   <>
+      <LargeLogo variant="password-rest" />
+      <Form variant="password-rest" />
+   </>
 );
 
 PasswordReset.getLayout = (page: ReactElement) => (
-  <SignLayout question="Already have an account" suggestion="Log in" to="/login">
-    {page}
-  </SignLayout>
+   <SignLayout question="Already have an account" suggestion="Log in" to="/login">
+      {page}
+   </SignLayout>
 );
 
 export default PasswordReset;
