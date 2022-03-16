ca9541e95abf0569acbfa7e75f091bf221184cf0
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-03-16 00:05:59 +0000

    added type keyword

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,19 +1,19 @@
-import React, { FormEvent, ReactElement, useRef, useState } from "react";
+import type { ReactElement } from "react";
 import SignLayout from "../layouts/SignLayout";
 import LargeLogo from "../components/common/LoginSignup/LargeLogo";
 import Form from "../components/common/LoginSignup/Form";
 
 const Login = () => (
-	<>
-		<LargeLogo variant="login" />
-		<Form variant="login" />
-	</>
+   <>
+      <LargeLogo variant="login" />
+      <Form variant="login" />
+   </>
 );
 
 Login.getLayout = (page: ReactElement) => (
-	<SignLayout question="New to jobs" suggestion="Create an account" to="/signup">
-		{page}
-	</SignLayout>
+   <SignLayout question="New to jobs" suggestion="Create an account" to="/signup">
+      {page}
+   </SignLayout>
 );
 
 export default Login;
