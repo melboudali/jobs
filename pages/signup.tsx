2ff48dbe8b22e0bbb02e60dfede1949ee0eff0d0
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-03-16 00:02:47 +0000

    added type keyword

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,21 +1,20 @@
 import type { MyNextPage } from "next";
-import PropTypes from "prop-types";
-import { ReactElement } from "react";
+import type { ReactElement } from "react";
 import Form from "../components/common/LoginSignup/Form";
 import LargeLogo from "../components/common/LoginSignup/LargeLogo";
 import SignLayout from "../layouts/SignLayout";
 
 const Signup: MyNextPage = () => (
-    <>
-        <LargeLogo variant="signup" />
-        <Form variant="signup" />
-    </>
+   <>
+      <LargeLogo variant="signup" />
+      <Form variant="signup" />
+   </>
 );
 
 Signup.getLayout = (page: ReactElement) => (
-    <SignLayout question="Already have an account" suggestion="Log in" to="/login">
-        {page}
-    </SignLayout>
+   <SignLayout question="Already have an account" suggestion="Log in" to="/login">
+      {page}
+   </SignLayout>
 );
 
 export default Signup;
