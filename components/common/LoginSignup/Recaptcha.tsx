e44f913078d8c1a8938a3125db2878b463300bcc
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-03-20 00:44:53 +0000

    added recaptchaRef

--- components/common/LoginSignup/Recaptcha.tsx
+++ components/common/LoginSignup/Recaptcha.tsx
@@ -1,28 +1,25 @@
-import { useRef } from "react";
+import { type RefObject, useRef } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 import styles from "./Recaptcha.module.scss";
 
 interface Props {
-	sitekey: string;
-	setRecaptcha: (value: string) => void;
+   sitekey: string;
+   recaptchaRef: RefObject<ReCAPTCHA>;
+   setRecaptcha: (value: string) => void;
 }
 
-const Recaptcha = ({ sitekey, setRecaptcha }: Props) => {
-	const recaptchaRef = useRef<ReCAPTCHA>(null);
-
-	return (
-		<ReCAPTCHA
-			className={styles.recaptcha}
-			ref={recaptchaRef}
-			sitekey={sitekey}
-			onChange={(value: string | null) => {
-				if (!value) {
-					return null;
-				}
-				setRecaptcha(value);
-			}}
-		/>
-	);
-};
+const Recaptcha = ({ sitekey, recaptchaRef, setRecaptcha }: Props) => (
+   <ReCAPTCHA
+      className={styles.recaptcha}
+      ref={recaptchaRef}
+      sitekey={sitekey}
+      onChange={(value: string | null) => {
+         if (!value) {
+            return null;
+         }
+         setRecaptcha(value);
+      }}
+   />
+);
 
 export default Recaptcha;
