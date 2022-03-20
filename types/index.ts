c0141009507141cd1deaa19b00070cec9970ee44
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-03-20 00:48:18 +0000

    added types

--- types/index.ts
+++ types/index.ts
@@ -1,7 +1,8 @@
 import { object, number, string, ObjectSchema } from "yup";
-import { ObjectShape, OptionalObjectSchema } from "yup/lib/object";
+import { AssertsShape, ObjectShape, OptionalObjectSchema } from "yup/lib/object";
 import type { RequiredStringSchema } from "yup/lib/string";
 import type { AnyObject } from "yup/lib/types";
+import { User } from "./react-redux";
 
 export interface testimonial {
    id: number;
@@ -34,9 +35,36 @@ export interface useFormValues {
 }
 
 // Yup
+export type FieldError = {
+   field?: Fields;
+   message: string;
+};
+
+export type UserValidationRes = AssertsShape<{
+   lastName?: RequiredStringSchema<string | undefined, AnyObject>;
+   firstName?: RequiredStringSchema<string | undefined, AnyObject>;
+   password?: RequiredStringSchema<string | undefined, AnyObject>;
+   email: RequiredStringSchema<string | undefined, AnyObject>;
+   recaptcha: RequiredStringSchema<string | undefined, AnyObject>;
+}>;
+
 export interface YupError {
-   message: { field: string; message: string } | string;
+   message: FieldError | string;
 }
 
 // Form, login, signup ans password-reset
+export type Fields = "firstName" | "lastName" | "email" | "password" | "recaptcha";
 export type Variant = "login" | "signup" | "password-rest";
+
+export interface Response {
+   ok: boolean;
+   error: FieldError | null;
+}
+
+export interface ValidateFuncResponse extends Response {
+   user: UserValidationRes | null;
+}
+
+export interface SignUpAndLoginResponse extends Response {
+   nUser: User | null;
+}
