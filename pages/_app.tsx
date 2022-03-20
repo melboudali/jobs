fddaee37b6761ef49fd3698e36524836a15713e0
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-03-20 00:47:28 +0000

    added react-redux provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
 import type { ReactNode } from "react";
 import type { AppPropsWithLayout } from "next/app";
+import store from "../lib/react-redux/store";
+import { Provider } from "react-redux";
 import "../styles/globals.scss";
 
 const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
    const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
-   return getLayout(<Component {...pageProps} />);
+   return getLayout(
+      <Provider store={store}>
+         <Component {...pageProps} />
+      </Provider>
+   );
 };
 
 export default MyApp;
