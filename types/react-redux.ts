dfc9a264867e05843c1d7ae9d1245880e2aa7e3f
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-03-22 23:09:32 +0000

    added UserReducer

--- types/react-redux.ts
+++ types/react-redux.ts
@@ -1,7 +1,16 @@
+import { FieldError } from ".";
+
 export interface User {
    displayName: string;
    firstName: string;
    lastName: string;
    email: string;
    photoURL: string;
+   date?: string;
+}
+
+export interface UserReducer {
+   value: User;
+   loading: boolean;
+   error: FieldError | null;
 }
