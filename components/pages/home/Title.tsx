ed16e45d4a52105525985e87ad248d9ff700fa88
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-03-27 23:10:43 +0100

    react.memo added

--- components/pages/home/Title.tsx
+++ components/pages/home/Title.tsx
@@ -1,6 +1,6 @@
-import type { FC } from "react";
+import { type FC, memo } from "react";
 import styles from "../../../pages/index.module.scss";
 
 const Title: FC = ({ children }) => <h2 className={styles.title}>{children}</h2>;
 
-export default Title;
+export default memo<FC>(Title);
