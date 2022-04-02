4caf1227c6fe35ca0ec7d13026b5172da2a78c03
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-04-03 00:55:08 +0100

    chore: trying to fix some bugs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,12 @@
 import type { ReactNode } from "react";
 import type { AppPropsWithLayout } from "next/app";
-import store from "../lib/react-redux/store";
+import store from "../react-redux/store";
 import { Provider } from "react-redux";
 import "../styles/globals.scss";
 
 const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
    const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
-   return getLayout(
-      <Provider store={store}>
-         <Component {...pageProps} />
-      </Provider>
-   );
+   return <Provider store={store}>{getLayout(<Component {...pageProps} />)}</Provider>;
 };
 
 export default MyApp;
