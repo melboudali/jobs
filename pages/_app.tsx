7d9ae56b41d01099b8d7e83e49930954b7744659
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-04-03 23:13:22 +0100

    feat: add Auth comp

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,37 @@
-import type { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import type { AppPropsWithLayout } from "next/app";
-import store from "../react-redux/store";
+import store, { useDispatch } from "../react-redux/store";
 import { Provider } from "react-redux";
 import "../styles/globals.scss";
 
+import { myAuth } from "../react-redux/features/userSlice";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../lib/firebase";
+
+const Auth = () => {
+   const dispatch = useDispatch();
+
+   useEffect(() => {
+      const unsubscribe = onAuthStateChanged(auth, async user => {
+         dispatch(myAuth(user));
+         console.log("rendered");
+      });
+
+      return () => {
+         unsubscribe();
+      };
+   }, [dispatch]);
+   return null;
+};
+
 const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
-   const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
-   return <Provider store={store}>{getLayout(<Component {...pageProps} />)}</Provider>;
+   const getLayout = Component.getLayout || ((page: ReactNode) => page);
+   return getLayout(
+      <Provider store={store}>
+         <Auth />
+         <Component {...pageProps} />
+      </Provider>
+   );
 };
 
 export default MyApp;
