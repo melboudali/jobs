2ffe9607aafbf3e60d8d680a1ebea03b2e1c4767
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-04-05 00:31:00 +0100

    feat: add redux hooks

--- layouts/SignLayout.tsx
+++ layouts/SignLayout.tsx
@@ -1,6 +1,11 @@
+import Error from "next/error";
+import { Router } from "next/router";
+import { useReducer } from "react";
 import Footer from "../components/common/LoginSignup/Footer";
 import withAuth from "../hoc/withAuth";
 import withoutAuth from "../hoc/withoutAuth";
+import { authenticatedSelector, isLoadingSelector } from "../react-redux/features/userSlice";
+import { useSelector } from "../react-redux/store";
 import styles from "./Layouts.module.scss";
 
 interface Props {
@@ -9,11 +14,26 @@ interface Props {
    to: "/login" | "/signup";
 }
 
-const SignLayout: React.FC<Props> = ({ children, question, suggestion, to }) => (
-   <>
-      <main className={styles.signMain}>{children}</main>
-      <Footer question={question} suggestion={suggestion} to={to} />
-   </>
-);
+const SignLayout: React.FC<Props> = ({ children, question, suggestion, to }) => {
+   const authenticated = useSelector(authenticatedSelector);
+   const isLoading = useSelector(isLoadingSelector);
+
+   if (isLoading) {
+      return <p>Loading ...</p>;
+   }
+
+   if (authenticated) {
+      // Router.push("/app");
+      //  return null;
+      return <Error statusCode={404} />;
+   }
+
+   return (
+      <>
+         <main className={styles.signMain}>{children}</main>
+         <Footer question={question} suggestion={suggestion} to={to} />
+      </>
+   );
+};
 
 export default SignLayout;
