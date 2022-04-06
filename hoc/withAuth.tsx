f11fe05c3091aaf36ee3e4b361f97fee061fce6e
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-04-07 00:40:53 +0100

    feat: add auth HOC

--- hoc/withAuth.tsx
+++ hoc/withAuth.tsx
@@ -1,24 +1,23 @@
-import { NextComponentType, MyNextPage } from "next";
-import Error from "next/error";
-import Router from "next/router";
-import { useSelector } from "react-redux";
-import { authenticatedSelector, selectUser, statusSelector, userSelector } from "../react-redux/features/userSlice";
+import type { MyNextPage } from "next";
+import { useRouter } from "next/router";
+import { useSelector } from "@redux/store";
+import { authenticatedSelector, statusSelector } from "@redux/features/userSlice";
+import Loading from "@components/common/Loading";
 
-function withAuth<T>(Component: MyNextPage<T>) {
+function withAuth<T, U>(isPublic: U, Component: MyNextPage<T>) {
    const Auth = (props: T) => {
-      const user = useSelector(userSelector);
-      const status = useSelector(statusSelector);
+      const router = useRouter();
       const authenticated = useSelector(authenticatedSelector);
-      const loading = status === "loading";
+      const status = useSelector(statusSelector);
+      const [isAuth, route] = isPublic ? [authenticated, "/app"] : [!authenticated, "/login"];
 
-      if (loading) {
-         return <p>Loading ...</p>;
+      if (status === "loading" || status === "idle") {
+         return <Loading />;
       }
 
-      if (!authenticated && !loading) {
-         Router.push("/login");
+      if (isAuth) {
+         router.push(route);
          return null;
-         //  return <Error statusCode={404} />;
       }
 
       return <Component {...props} />;
@@ -26,7 +25,7 @@ function withAuth<T>(Component: MyNextPage<T>) {
 
    // Copy getInitial props so it will run as well
    // if (Component.getInitialProps) {
-   // 	Auth.getInitialProps = Component.getInitialProps;
+   //    Auth.getInitialProps = Component.getInitialProps;
    // }
 
    return Auth;
