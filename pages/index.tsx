4c2baed1fcfdaa2dfe2a6e4847538a5f68089727
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-04-07 00:49:21 +0100

    feat: add module path aliases

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,17 @@
 import type { ReactNode } from "react";
 import type { GetStaticProps } from "next";
 import Head from "next/head";
-import Layout from "../layouts/Layout";
-import Hero from "../components/pages/home/Hero";
-import Jobs from "../components/pages/home/Jobs";
-import Callouts from "../components/pages/home/Callouts";
-import Stats from "../components/pages/home/Stats";
-import Testimonials from "../components/pages/home/Testimonials";
-import Posts from "../components/pages/home/Posts";
-import Icons from "../components/pages/home/Icons";
+import Layout from "@layouts/Layout";
+import Hero from "@components/pages/home/Hero";
+import Jobs from "@components/pages/home/Jobs";
+import Callouts from "@components/pages/home/Callouts";
+import Stats from "@components/pages/home/Stats";
+import Testimonials from "@components/pages/home/Testimonials";
+import Posts from "@components/pages/home/Posts";
+import Icons from "@components/pages/home/Icons";
 import styles from "./index.module.scss";
-import type { Posts as PostsType } from "../types";
-import { getSortedPostsData } from "../lib/blog";
+import type { Posts as PostsType } from "@globalTypes";
+import { getSortedPostsData } from "@lib/blog";
 
 interface Props {
    posts: PostsType;
