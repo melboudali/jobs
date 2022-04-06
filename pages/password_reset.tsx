c66a7c51d1467740fbe78081dc7a1acf4c629ab7
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-04-07 00:50:03 +0100

    feat: add module path aliases

--- pages/password_reset.tsx
+++ pages/password_reset.tsx
@@ -1,21 +1,23 @@
-import type { MyNextPage } from "next";
 import type { ReactElement } from "react";
-import Form from "../components/common/LoginSignup/Form";
-import LargeLogo from "../components/common/LoginSignup/LargeLogo";
-import withoutAuth from "../hoc/withoutAuth";
-import SignLayout from "../layouts/SignLayout";
+import SignLayout from "@layouts/SignLayout";
+import type { SignLayoutProps } from "@globalTypes";
+import LargeLogo from "@components/common/LoginSignup/LargeLogo";
+import Form from "@components/common/LoginSignup/Form";
 
-const PasswordReset: MyNextPage = () => (
+const PasswordReset = () => (
    <>
       <LargeLogo variant="password-rest" />
       <Form variant="password-rest" />
    </>
 );
 
-PasswordReset.getLayout = (page: ReactElement) => (
-   <SignLayout question="Already have an account" suggestion="Log in" to="/login">
-      {page}
-   </SignLayout>
-);
+PasswordReset.getLayout = (page: ReactElement) => {
+   const Props: Omit<SignLayoutProps, "children"> = {
+      question: "Already have an account",
+      suggestion: "Log in",
+      to: "/login",
+   };
+   return <SignLayout {...Props}>{page}</SignLayout>;
+};
 
 export default PasswordReset;
