831cc8f3207269564db7a6b275bc9f0e431efc17
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-04-09 23:58:57 +0100

    feat: add navbar animation

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import cls from "classnames";
-import data from "@data/navbarLinks.json";
+import LinksData from "@data/navbarLinks.json";
 import Navar from "./Navbar";
 import Links from "./Navbar/Links";
 import styles from "./Header.module.scss";
+import { useSelector } from "@redux/store";
+import { isAuthenticatedSelector } from "@redux/features/userSlice";
+import useNavbar from "@hooks/useNavbar";
 
 const Header = () => {
+   const isAthenticated = useSelector(isAuthenticatedSelector);
+   const data = [...LinksData, { name: "Start Free", href: isAthenticated ? "/app" : "/login" }];
+   const { scrollDown } = useNavbar(47);
    const [showHamburgerMenu, setShowHamburgerMenu] = useState(false);
 
    const toggle = () => {
@@ -13,11 +19,15 @@ const Header = () => {
       setShowHamburgerMenu(!showHamburgerMenu);
    };
 
-   const headerStyles = cls(styles.header, { [styles["header--open"]]: showHamburgerMenu });
+   const headerStyles = cls(styles.header, {
+      [styles["header--open"]]: showHamburgerMenu,
+      [styles["header--show"]]: scrollDown,
+      [styles["header--hide"]]: !scrollDown,
+   });
 
    return (
       <header className={headerStyles}>
-         <Navar showHamburgerMenu={showHamburgerMenu} toggle={toggle} />
+         <Navar data={data} showHamburgerMenu={showHamburgerMenu} toggle={toggle} />
          {showHamburgerMenu && <Links variant="mobile" data={data} showHamburgerMenu={showHamburgerMenu} />}
       </header>
    );
