7f1d5151dadd2ba7323c29c5cd0cde8648b5076b
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-04-14 23:07:18 +0000

    refactor: add images domain

--- next.config.js
+++ next.config.js
@@ -2,11 +2,11 @@
 const path = require("path");
 
 module.exports = {
-	reactStrictMode: true,
-	images: {
-		domains: ["inkbotdesign.com", "1000logos.net"]
-	},
-	sassOptions: {
-		includePaths: [path.join(__dirname, "styles")]
-	}
+   reactStrictMode: true,
+   images: {
+      domains: ["inkbotdesign.com", "1000logos.net", "static.remove.bg", "cdn-icons-png.flaticon.com"],
+   },
+   sassOptions: {
+      includePaths: [path.join(__dirname, "styles")],
+   },
 };
