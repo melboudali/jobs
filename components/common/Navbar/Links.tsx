8541f2526650bc4dfb82cbc86e7eec686128cd99
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-04-20 23:12:00 +0000

    refactor: rename styles file

--- components/common/Navbar/Links.tsx
+++ components/common/Navbar/Links.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import cls from "classnames";
 import { navbarProps } from "@customTypes/index";
-import styles from "./Navbar.module.scss";
+import styles from "./index.module.scss";
 
 interface Props extends navbarProps {
    variant: "desktop" | "mobile";
