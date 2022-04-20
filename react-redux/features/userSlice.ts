104325e943bb3d9d69b95916b221758f28a2c5ce
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-04-20 23:53:09 +0000

    feat: add updateUser reducer and remove selectors

--- react-redux/features/userSlice.ts
+++ react-redux/features/userSlice.ts
@@ -1,105 +1,15 @@
-import { AsyncThunk, createAsyncThunk, createSelector, createSlice, isAnyOf, type PayloadAction } from "@reduxjs/toolkit";
-import { signInWithEmailAndPassword, createUserWithEmailAndPassword, User, sendPasswordResetEmail, signOut } from "firebase/auth";
-import { doc, getDoc, setDoc } from "firebase/firestore";
-import Firestore, { auth } from "@firebase";
-import { RootState } from "@redux/store";
-import { Codes, FieldError, useFormValues } from "@globalTypes";
-import type { User as UserType, UserReducer } from "@customTypes/react-redux";
-import FormSubmit from "@utils/FormSubmit";
+import { AsyncThunk, createSlice, isAnyOf } from "@reduxjs/toolkit";
+import type { UserReducer } from "@customTypes/react-redux";
+import { login, myAuth, passwordReset, signOutThunk, signUp } from "@redux/reducers";
+import { updateUser } from "./userSlice with selectors and reducers";
 
 type GenericAsyncThunk = AsyncThunk<unknown, unknown, any>;
 type PendingAction = ReturnType<GenericAsyncThunk["pending"]>;
 type FulfilledAction = ReturnType<GenericAsyncThunk["fulfilled"]>;
 
-const formSubmit = new FormSubmit();
-
-export const login = createAsyncThunk(
-   "user/login",
-   async ({ email, password }: Required<Pick<useFormValues, "email" | "password">>, thunkAPI) => {
-      try {
-         const userCredential = await signInWithEmailAndPassword(auth, email, password);
-         const querySnapshot = await getDoc(doc(Firestore, "users", userCredential.user.uid));
-         return { ...querySnapshot.data(), date: new Date().toISOString() };
-      } catch (error) {
-         const { code } = error as FieldError;
-         const errorMessage = formSubmit.firebaseErrors(code as keyof Codes);
-         return thunkAPI.rejectWithValue(errorMessage);
-      }
-   }
-);
-
-export const signUp = createAsyncThunk(
-   "user/signUp",
-   async ({ firstName, lastName, email, password }: Required<useFormValues>, thunkAPI) => {
-      try {
-         const newUser: UserType = {
-            displayName: firstName + " " + lastName,
-            firstName: firstName,
-            lastName: lastName,
-            email: email,
-            photoURL: "https://cdn-icons-png.flaticon.com/512/149/149071.png",
-            type: null,
-            date: new Date().toISOString(),
-         };
-
-         const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-         await setDoc(doc(Firestore, "users", userCredential.user.uid), newUser);
-         return newUser;
-      } catch (error) {
-         const { code } = error as FieldError;
-         const errorMessage = formSubmit.firebaseErrors(code as keyof Codes);
-         return thunkAPI.rejectWithValue(errorMessage);
-      }
-   }
-);
-
-export const myAuth = createAsyncThunk("user/myAuth", async (user: User | null, thunkAPI) => {
-   try {
-      if (!user) {
-         throw { code: "auth/user-not-found" } as FieldError;
-      }
-
-      const querySnapshot = await getDoc(doc(Firestore, "users", user.uid));
-      const value: UserType = {
-         displayName: querySnapshot.data()?.displayName,
-         firstName: querySnapshot.data()?.firstName,
-         lastName: querySnapshot.data()?.lastName,
-         email: querySnapshot.data()?.email,
-         photoURL: user.photoURL ?? querySnapshot.data()?.photoURL,
-         type: querySnapshot.data()?.type,
-         date: new Date().toISOString(),
-      };
-      return value;
-   } catch (error) {
-      const { code } = error as FieldError;
-      const errorMessage = formSubmit.firebaseErrors(code as keyof Codes);
-      return thunkAPI.rejectWithValue(errorMessage);
-   }
-});
-
-export const passwordReset = createAsyncThunk("user/psswordReset", async (email: string, thunkAPI) => {
-   try {
-      // await sendPasswordResetEmail(auth, email);
-      console.log("email reseting fired for the email: ", email);
-   } catch (error) {
-      const { code, message } = error as FieldError;
-      console.log(code, message);
-      return thunkAPI.rejectWithValue(message);
-   }
-});
-
-export const signOutThunk = createAsyncThunk("user/signOut", async (_, thunkAPI) => {
-   try {
-      await signOut(auth);
-   } catch (error) {
-      const { code, message } = error as FieldError;
-      console.log(code, message);
-      return thunkAPI.rejectWithValue(message);
-   }
-});
-
 const initialState: UserReducer = {
    value: {
+      id: "",
       displayName: "",
       firstName: "",
       lastName: "",
@@ -138,11 +48,11 @@ export const userSlice = createSlice({
             state.error = { variant: "password-rest", message: action.payload as string };
          })
          .addCase(signOutThunk.fulfilled, () => ({ ...initialState, isFetching: false, status: "succeeded" }))
-         .addMatcher(isAnyOf(login.pending, signUp.pending, signOutThunk.pending), state => {
+         .addMatcher(isAnyOf(login.pending, signUp.pending, signOutThunk.pending, updateUser.pending), state => {
             state.status = "loading";
             state.error = null;
          })
-         .addMatcher(isAnyOf(login.fulfilled, signUp.fulfilled, myAuth.fulfilled), (state, action) => {
+         .addMatcher(isAnyOf(login.fulfilled, signUp.fulfilled, myAuth.fulfilled, updateUser.fulfilled), (state, action) => {
             state.isFetching = false;
             state.isAuthenticated = true;
             state.status = "succeeded";
@@ -152,21 +62,3 @@ export const userSlice = createSlice({
 });
 
 export default userSlice.reducer;
-
-export const selectUser: (state: RootState) => UserReducer = state => state.user;
-
-export const isAuthenticatedSelector = createSelector(selectUser, user => user.isAuthenticated);
-
-export const errorSelector = createSelector(selectUser, user => user.error);
-
-export const userSelector = createSelector(selectUser, user => user.value);
-
-export const statusSelector = createSelector(selectUser, user => user.status);
-
-export const isLoadingSelector = createSelector(selectUser, user => user.status === "loading");
-
-export const errorVariantSelector = createSelector(selectUser, user => user.error?.variant);
-
-export const isFetchingSelector = createSelector(selectUser, user => user.isFetching);
-
-export const typeSelector = createSelector(selectUser, user => user.value.type);
