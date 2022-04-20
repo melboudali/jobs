a334d00d30c36f13da7d9c4b6e084067302f52e5
Moe EL BOUDALI <med.sneaky@gmail.com>
2022-04-20 23:50:06 +0000

    refactor: rename file

---
+++ layouts/App.layout.tsx
@@ -0,0 +1,25 @@
+import type { ReactNode } from "react";
+import withAuth from "@hoc/withAuth";
+import Navbar from "@components/pages/app/Navbar";
+import styles from "./Layouts.module.scss";
+import { useSelector } from "@redux/store";
+import { isFetchingSelector, typeSelector } from "@redux/selectors";
+import FirstTimeVisit from "@components/pages/app/FirstTimeVisit";
+
+interface Props {
+   children: ReactNode;
+}
+
+const AppLayout = ({ children }: Props) => {
+   const userType = useSelector(typeSelector);
+   const isFetching = useSelector(isFetchingSelector);
+
+   return (
+      <div className={styles.app}>
+         <Navbar />
+         <main className={styles.app__main}>{userType ? children : <FirstTimeVisit />}</main>
+      </div>
+   );
+};
+
+export default withAuth(false, AppLayout);
